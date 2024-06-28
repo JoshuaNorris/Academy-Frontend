560a318f8a0c3719ddd135a10641b5c81d24647f
Joshua Norris <joshua.norris52@gmail.com>
2024-06-28 10:48:32 -0500

    Fixing build errors

--- src/pages/BookOverviewPage/TopicComponent.tsx
+++ src/pages/BookOverviewPage/TopicComponent.tsx
@@ -6,27 +6,27 @@ interface TopicProps {
 
 const TopicComponent: React.FC<TopicProps> = ({TopicName}) => {
     return (
-        <div class="my-10 px-4 sm:px-8 max-w-5xl m-auto">
+        <div className="my-10 px-4 sm:px-8 max-w-5xl m-auto">
             <h1 className="text-center font-semibold text-lg">{TopicName}</h1>
-            <ul class="border border-gray-200 rounded overflow-hidden shadow-md mt-5">
-                <li class="px-4 py-2 bg-white hover:bg-sky-100 hover:text-sky-900 border-b last:border-none border-gray-200 transition-all duration-300 ease-in-out">
+            <ul className="border border-gray-200 rounded overflow-hidden shadow-md mt-5">
+                <li className="px-4 py-2 bg-white hover:bg-sky-100 hover:text-sky-900 border-b last:border-none border-gray-200 transition-all duration-300 ease-in-out">
                     <a href="/Academy-Frontend/book/title:BookTitle">
                         First Item
                     </a>
                 </li>
-                <li class="px-4 py-2 bg-white hover:bg-sky-100 hover:text-sky-900 border-b last:border-none border-gray-200 transition-all duration-300 ease-in-out">
+                <li className="px-4 py-2 bg-white hover:bg-sky-100 hover:text-sky-900 border-b last:border-none border-gray-200 transition-all duration-300 ease-in-out">
                     <a href="/Academy-Frontend/book/title:BookTitle">
                         Second Item
                     </a>
-                </li>                <li class="px-4 py-2 bg-white hover:bg-sky-100 hover:text-sky-900 border-b last:border-none border-gray-200 transition-all duration-300 ease-in-out">
+                </li>                <li className="px-4 py-2 bg-white hover:bg-sky-100 hover:text-sky-900 border-b last:border-none border-gray-200 transition-all duration-300 ease-in-out">
                     <a href="/Academy-Frontend/book/title:BookTitle">
                         Third Item
                     </a>
-                </li>                <li class="px-4 py-2 bg-white hover:bg-sky-100 hover:text-sky-900 border-b last:border-none border-gray-200 transition-all duration-300 ease-in-out">
+                </li>                <li className="px-4 py-2 bg-white hover:bg-sky-100 hover:text-sky-900 border-b last:border-none border-gray-200 transition-all duration-300 ease-in-out">
                     <a href="/Academy-Frontend/book/title:BookTitle">
                         Fourth Item
                     </a>
-                </li>                <li class="px-4 py-2 bg-white hover:bg-sky-100 hover:text-sky-900 border-b last:border-none border-gray-200 transition-all duration-300 ease-in-out">
+                </li>                <li className="px-4 py-2 bg-white hover:bg-sky-100 hover:text-sky-900 border-b last:border-none border-gray-200 transition-all duration-300 ease-in-out">
                     <a href="/Academy-Frontend/book/title:BookTitle">
                         Fifth Item
                     </a>
